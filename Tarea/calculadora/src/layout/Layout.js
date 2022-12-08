736a38d82bf83cc2fd7b8532540cb3352f2010c9
ramons03 <git@ramons.digital>
2022-12-08 20:06:57 -0300

    cambios en css

--- Tarea/calculadora/src/layout/Layout.js
+++ Tarea/calculadora/src/layout/Layout.js
@@ -1,11 +1,9 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../contexts/ThemeContext";
+import React, {  } from "react";
 import "./layout.scss";
 var Layout = function (_a) {
     var children = _a.children;
-    var theme = useContext(ThemeContext).theme;
     return (React.createElement("div", { className: "layout-container" },
-        React.createElement("div", { className: theme },
+        React.createElement("div", { className: "first-theme" },
             React.createElement("div", { className: "calculator-container" }, children))));
 };
 export default Layout;
